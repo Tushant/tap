8cc901e4e05e59633b7c17879c6b84e606858de7
Tushar Khatiwada <khatiwadatushar@outlook.com>
2017-06-07 20:01:38 +0545

    Add some stylings

--- src/components/Campaigns/personas.js
+++ src/components/Campaigns/personas.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
-export const Personas = (props) => {
-  const renderPersonas = props.personas && props.personas.map((persona, i) => {
-    return <div key={i}>{persona.personas && persona.personas.map((person, i) => {
-      return (
-        <div key={i*2}>
-          <input type="checkbox" />
-          <li>{person.name}</li>
-        </div>
-      )
-    })}</div>
-  })
-  return (
-    <ReactModal
-      isOpen={props.showModal}
-      onRequestClose={props.hideModal}
-      contentLabel="Modal"
-    >
-      <h1>Personas</h1>
-      {renderPersonas && renderPersonas}
-    </ReactModal>
-  );
+export const Personas = props => {
+	const renderPersonas =
+		props.personas &&
+		props.personas.map((persona, i) => {
+			console.log(persona);
+			return (
+				<div key={i}>
+					{persona.personas &&
+						persona.personas.map((person, i) => {
+							return (
+								<div key={i * 2}>
+									<input type="checkbox" />
+									<li>{person.name}</li>
+								</div>
+							);
+						})}
+				</div>
+			);
+		});
+	return (
+		<ReactModal
+			isOpen={props.showModal}
+			onRequestClose={props.hideModal}
+			contentLabel="Modal"
+		>
+			<h1>Personas</h1>
+			{renderPersonas && renderPersonas}
+		</ReactModal>
+	);
 };
