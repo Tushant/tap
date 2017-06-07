8d9718fb6d9e48650e8a08fbf198c0cef96f2dd3
Tushar Khatiwada <khatiwadatushar@outlook.com>
2017-06-07 19:44:47 +0545

    Add some stylings

--- src/components/Campaigns/CreateNewAd.js
+++ src/components/Campaigns/CreateNewAd.js
@@ -6,38 +6,47 @@ import Collapsible from 'react-collapsible';
 import { Personas } from './personas';
 
 const adsTitle = () => {
-  return (
-    <div className="adTitle">
-      <FaAngleDown />
-      <span className="adTitle">
-        <Column small={3}>
-          <input type="text" />
-        </Column>
-      </span>
-      <span className="adMatches">ad matches</span>
-    </div>
-  );
+	return (
+		<Row className="adTitle expanded">
+			<FaAngleDown />
+			<Column small={3} className="adTitleInput">
+				<input type="text" placeholder="type ad text here" autoFocus />
+			</Column>
+			<Column small={3}>
+				<span className="adMatches">ad matches</span>
+			</Column>
+		</Row>
+	);
 };
 
-const CreateNewAd = (props) => {
-  // const renderPersonas = props.personas && props.personas.map((persona, i) => {
-  //   return <div key={i}>{persona.personas && persona.personas.map((person, i) => <li key={i}>{JSON.stringify(person)}</li>)}</div>
-  // });
+const CreateNewAd = props => {
+	// const renderPersonas = props.personas && props.personas.map((persona, i) => {
+	//   return <div key={i}>{persona.personas && persona.personas.map((person, i) => <li key={i}>{JSON.stringify(person)}</li>)}</div>
+	// });
 
-  return (
-    <Collapsible trigger={adsTitle()} classParentString="adsCollapse" open>
-      <Row>
-        <Column small={12}><textarea /></Column>
-        <Button>Edit</Button>
-      </Row>
-      <p>
-        Personas
-        {' '}
-        <span><FaPlusCircle onClick={props.personasClick} /></span>
-        {props.personasShow ? <Personas showModal={props.showModal} hideModal={props.hideModal} personas={props.personas} /> : null}
-      </p>
-    </Collapsible>
-  );
+	return (
+		<Collapsible trigger={adsTitle()} classParentString="adsCollapse" open>
+			<Row className="expanded">
+				<Column small={12}>
+					<h3>Ad</h3>
+					<textarea />
+				</Column>
+				<Button style={{ marginLeft: 20 }}>Edit</Button>
+			</Row>
+			<p style={{ marginLeft: 20 }}>
+				Personas
+				{' '}
+				<span><FaPlusCircle onClick={props.personasClick} /></span>
+				{props.personasShow
+					? <Personas
+							showModal={props.showModal}
+							hideModal={props.hideModal}
+							personas={props.personas}
+						/>
+					: null}
+			</p>
+		</Collapsible>
+	);
 };
 
 export default CreateNewAd;
